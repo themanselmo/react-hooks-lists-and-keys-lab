e59a4ac6c6e87249d438cc0ea73960dd5242b722
max anselmo <itmanselmo@gmail.com>
2021-10-13 11:11:12 -0400

    not sure why I'm failing the test for not assigning each projectItem with a unique key of their id, aside from that, project completed: dynamically added projectItem components onto the page from the user.js file in the data folder

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  /* display an <a> tag for each link here */
+  return <nav>
+    <a href='#home'>home</a>
+    <a href='#about'>about</a>
+    <a href='#projects'>projects</a>
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,22 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectList = projects.map(project => {
+    return <ProjectItem 
+              name={project.name} 
+              about={project.about} 
+              technologies={project.technologies}
+              id={project.id}
+            />
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+        {projectList}
+      </div>
     </div>
   );
 }
